3d006c0fa7af5592b0d7c3b8d943ba4b685d4074
Clara LAVIALE <clara.laviale@gmail.com>
2025-04-07 13:55:36 +0200

    affichage de la salle avec les tables occupées ou non

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/global/header/header.component";
 import { FooterComponent } from "./components/global/footer/footer.component";
+import { SalleComponent } from "./components/commande/salle/salle.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SalleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/commande/salle/salle.component.ts
+++ src/app/components/commande/salle/salle.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableRestaurant } from '../../../interfaces/table-restaurant';
+import { TableRestaurantService } from '../../../services/table-restaurant.service';
+import { CommonModule } from '@angular/common';
+import { TableItemComponent } from './table-item/table-item.component';
 
 @Component({
   selector: 'app-salle',
-  imports: [],
+  imports: [CommonModule, TableItemComponent],
   templateUrl: './salle.component.html',
   styleUrl: './salle.component.css'
 })
-export class SalleComponent {
+export class SalleComponent implements OnInit{
+  tables: TableRestaurant[] = [];
+  idRestaurant = 1;
+
+  constructor(private tableService : TableRestaurantService) {}
+
+  ngOnInit(): void {
+    // D'abord, on récupère toutes les tables
+    this.tableService.get_tables(this.idRestaurant).subscribe(toutes => {
+      // Ensuite, on récupère les tables occupées
+      this.tableService.get_tables_occupees(this.idRestaurant).subscribe(occupees => {
+        const occupeesIds = occupees.map(t => t.id);
+        this.tables = toutes.map(table => ({
+          ...table,
+          estOccupee: occupeesIds.includes(table.id)
+        }));
+      });
+    });
+  }
 
 }
