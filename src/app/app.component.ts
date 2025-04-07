5f94704368be4db5755600c5616b398a45a50089
Jean Mugniery <jean.mugniery@gmail.com>
2025-04-07 09:57:55 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationEmployeComponent } from "./components/global/navigation-employe/navigation-employe.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationEmployeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
