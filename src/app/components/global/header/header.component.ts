958de6bc70db696c6a4f99c12c54b1103070ab72
Jean Mugniery <jean.mugniery@gmail.com>
2025-04-09 14:26:35 +0200

    retour arrière dynamique mis en place, changement des icones du header en fonction de la page, gestion du logout

--- src/app/components/global/header/header.component.ts
+++ src/app/components/global/header/header.component.ts
@@ -1,21 +1,46 @@
 import { Component} from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Location } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent{
   
-  last_url : string = "/login";
-
-  constructor (private location : Location) {}
-
-  goToLastUrl() : void {
-    this.location.back();
+  constructor (private location : Location,
+    private router : Router) {}
+    
+    goToLastUrl() : void {
+      this.location.back();
+    }
+    
+    redirectionNavigation() {
+      console.log(this.router.url);
+      
+      if(localStorage.getItem("id_restaurant") == "0") {
+        this.router.navigate(["/navigation-admin"]);
+      }
+      
+      if(localStorage.getItem("id_restaurant") != null && localStorage.getItem("id_restaurant") != "0") {
+        this.router.navigate(["/navigation-employe"]);
+      }
+    }
+    
+    isCurrentPageLogin() : boolean {
+      return this.router.url == "/login";
+    }
+    
+    isCurrentPageNavigation() : boolean {
+      return this.router.url.includes("navigation");
+    }
+    
+    logout() {
+      localStorage.removeItem("id_restaurant");
+      localStorage.removeItem("jwt");
+      this.router.navigate(['/login']);
+    }
   }
-
-}
+  
